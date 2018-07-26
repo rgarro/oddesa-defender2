e7bb0b25cd028e97cc337bb1835aa23723366f73
Rolando Garro <rolando@emptyart.xyz>
2018-07-26 11:42:01 -0700

    lunch truce at the front ...
    lunch truce at the front ...

--- src/OddesaDefender2/Lib/Lockheed.js
+++ src/OddesaDefender2/Lib/Lockheed.js
@@ -1,9 +1,27 @@
-//import React, { Component } from "react";
-//extends Component
+/**
+ *                            _
+ *                           | \
+ *                          _|  \______________________________________
+ *                         - ______        ________________          \_`,
+ *                       -(_______            -=    -=        USAF       )
+ *                                `--------=============----------------`   -JB
+ *                                          -   -
+ *                                         -   -
+ *                              `   . .  -  -
+ *                               .*` .* ;`*,`.,
+ *                                `, ,`.*.*. *
+ * _________________________________*  * ` ^ *____________________________
+ *        :::: Cloud Development is "Black" Magic ... ::::
+ */
+import * as THREE from "three";
+
 class Lockheed {
   constructor() {
-    this.meshName = "meshName";
-    this.modelPath = "modelPath";
+    this.meshName = "Spooky";
+    this.modelPath = "../Models/AC130/Models/AC130.obj";
+    this.vehicleMesh = null;
+    this.scale = 13;
+    this.group = new THREE.Object3D();
   }
 }
 
