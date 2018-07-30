4be51ea4aebfc77c11ce176fbd3ead419f787b69
Rolando Garro <rolando@emptyart.xyz>
2018-07-30 11:29:37 -0700

    橙色鲤鱼潜水离开
    橙色鲤鱼潜水离开

--- src/OddesaDefender2/Lib/Model.js
+++ src/OddesaDefender2/Lib/Model.js
@@ -1,9 +1,40 @@
 import * as THREE from "three";
+var FBXLoader = require("three-fbx-loader");
 
 class Model {
   constructor() {
     this.Game;
-    console.log("test inheritance");
+    this.game_is_set = false;
+    this.vehicleMesh = null;
+  }
+
+  loadModel(modelUrl) {
+    if (this.game_is_set) {
+      var loader = new THREE.JSONLoader();
+      loader.load(
+        modelUrl,
+        function(model, materials) {
+          var material = new THREE.MeshPhongMaterial();
+          //var material = new THREE.MeshBasicMaterial();
+          material.color.set(this.vehicleColor);
+          this.vehicleMesh = new THREE.Mesh(model, material);
+          this.vehicleMesh.name = this.vehicleMeshName;
+          this.vehicleMesh.scale.set(this.scale, this.scale, this.scale);
+          this.Game.Scene.add(this.vehicleMesh);
+          //this.vehicleMesh.rotation.y = -360;
+          this.postLoaded();
+        }.bind(this)
+      );
+    } else {
+      throw new Error("Needs a Game parent object");
+    }
+  }
+
+  postLoaded() {}
+
+  setParent(game) {
+    this.Game = game;
+    this.game_is_set = true;
   }
 }
 
