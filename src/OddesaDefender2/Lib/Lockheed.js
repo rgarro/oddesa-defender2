8d08748909d426312160457d0a2e39c99ac2e2b5
Rolando Garro <rgarro@gmail.com>
2018-08-08 14:10:04 -0700

    testing models ....

--- src/OddesaDefender2/Lib/Lockheed.js
+++ src/OddesaDefender2/Lib/Lockheed.js
@@ -20,8 +20,10 @@ class Lockheed extends Model {
   constructor() {
     super();
     this.meshName = "Spooky";
-    //this.modelPath = "/Models/AC130.fbx";
     this.modelPath = "/Models/ac130obje.fbx";
+    //this.modelPath = "/Models/tupolev.fbx";
+    //this.modelPath = "/Models/blackhawk.fbx";
+    //this.modelPath = "/Models/sikorsky.fbx";
     this.vehicleMesh = null;
     this.vehicleColor = 0x0FFA65;
     this.scale = 13;
