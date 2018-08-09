f3265784cbcbadafdd338eaafe5e67959fd45d8a
Rolando Garro <rgarro@gmail.com>
2018-08-09 12:47:12 -0700

    era coqueta en matagalpa, se le turquiaron unos chavalos ...

--- src/OddesaDefender2/Game.js
+++ src/OddesaDefender2/Game.js
@@ -13,6 +13,11 @@ class Game extends Component {
     this.Ac130h = new Lockheed();
     this.animate = {};
     this.enable_shadows = true;
+    this.rotationSpeed = 0.001;
+    this.ambientLightColor={};
+    this.clearColor = 0xa3e1ff;
+    this.floorTextureUrl = '/Textures/floor.png';
+    this.floorTexture = null;
   }
 
   render() {
@@ -85,6 +90,7 @@ class Game extends Component {
     //this.testSomethingIsOnTheScene();
     this.loadPlane();
     this.setLights();
+    this.floorAndSky()
   }
 
   loadPlane() {
@@ -103,18 +109,32 @@ class Game extends Component {
      this.Scene.add(ambientLight);
      var light = new THREE.PointLight(0xffffff);
      light.position.set(0,250,0);
-  /*if(this.enable_shadows){
+  if(this.enable_shadows){
     light.castShadow = true;
     light.shadow.camera.visible = true;
     light.shadow.camera.right     =  5;
     light.shadow.camera.left     = -5;
     light.shadow.camera.top      =  5;
     light.shadow.camera.bottom   = -5;
-  }*/
+  }
     this.Scene.add(light);
   }
 
-  floorAndSky() {}
+  floorAndSky() {
+    this.floorTexture = new THREE.TextureLoader().load(this.floorTextureUrl);
+  	this.floorTexture.wrapS = this.floorTexture.wrapT = THREE.RepeatWrapping;
+  	this.floorTexture.repeat.set( 10, 10 );
+  	var floorMaterial = new THREE.MeshBasicMaterial( { map: this.floorTexture, side: THREE.DoubleSide } );
+  	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+  	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.name = "floor";
+  	floor.position.y = -0.5;
+  	floor.rotation.x = Math.PI / 2;
+    if(this.enable_shadows){
+      floor.receiveShadow = true;
+    }
+  	this.Scene.add(floor);
+  }
 
   shouldComponentUpdate() {
     return false;
